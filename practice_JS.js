935ff2c67462fcceac3d665265d0bdb4a1a188aa
Trillion_Impulse <tkdduq2897@naver.com>
2025-03-18 21:51:03 +0900

    test(practice_JS.js): practice18

--- practice_JS.js
+++ practice_JS.js
@@ -1,1 +1,14 @@
-console.log("Hello JS");
+// console.log("Hello JS");
+var memberArray = ['aaa', 'bbb', 'ccc'];
+console.log("memberArray[0]", memberArray[0]);
+
+var memberObject = {
+    A:'aaa',
+    B:'bbb',
+    C:'Ccc'
+}
+memberObject.C = "ccc";
+console.log("memberObject.C", memberObject.C);
+console.log("memberObject.C", memberObject["C"]);
+delete memberObject.A
+console.log("after delete memberObject.A", memberObject.A);

956ee5f89b1f001dae054f867a87e38e1975ce90
Trillion_Impulse <tkdduq2897@naver.com>
2025-03-19 22:20:44 +0900

    test(practice_JS.js): practice19

--- practice_JS.js
+++ practice_JS.js
@@ -1,14 +1,26 @@
 // console.log("Hello JS");
 var memberArray = ['aaa', 'bbb', 'ccc'];
-console.log("memberArray[0]", memberArray[0]);
+// console.log("memberArray[0]", memberArray[0]);
+console.group('array loop');
+var i = 0;
+while(i < memberArray.length){
+    console.log(i, memberArray[i]);
+    i+=1;
+}
+console.groupEnd('array loop');
 
 var memberObject = {
     A:'aaa',
     B:'bbb',
     C:'Ccc'
 }
-memberObject.C = "ccc";
-console.log("memberObject.C", memberObject.C);
-console.log("memberObject.C", memberObject["C"]);
-delete memberObject.A
-console.log("after delete memberObject.A", memberObject.A);
+console.group('object loop');
+for(var name in memberObject){
+    console.log(name, memberObject[name]);
+}
+console.groupEnd('object loop');
+// memberObject.C = "ccc";
+// console.log("memberObject.C", memberObject.C);
+// console.log("memberObject.C", memberObject["C"]);
+// delete memberObject.A
+// console.log("after delete memberObject.A", memberObject.A);

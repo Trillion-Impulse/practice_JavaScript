f7747d6da4f92a9f7580f56c492b8021955fc4d7
Trillion_Impulse <tkdduq2897@naver.com>
2025-03-26 21:48:52 +0900

    test(practice_JS.js): practice32

--- practice_JS.js
+++ practice_JS.js
@@ -127,8 +127,10 @@ console.log('kim12.sum',kim12.sum());
 console.log('kim12.avg',kim12.avg());
 
 var superObj = {superVal: 'super'}
-var subObj = {subVal:'sub'}
-subObj.__proto__ = superObj;
+// var subObj = {subVal:'sub'}
+// subObj.__proto__ = superObj;
+var subObj = Object.create(superObj);
+subObj.subVal = 'sub';
 console.log('subObj.subVal: ',subObj.subVal);
 console.log('subObj.superVal: ',subObj.superVal);
 subObj.superVal ='sub';
